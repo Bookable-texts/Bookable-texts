bef39f837f28a2b5c97dd9559f47a413dca48a45
Mofeleng <mofelengmokgope@gmail.com>
2024-04-17 13:03:19 +0200

    Created the main layout for the page not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import PageNotFound from '../../public/PageNotFound.svg'
+import Link from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+    <section id="notFound">
+      <div className="container">
+        <div className="center">
+          <img src={PageNotFound} alt="Page not found SVG" className='page_not_found_img' />
+
+          <div className="content">
+            <h3 className="heading small">
+              Sorry we could not find the page you were looking for
+            </h3>
+            <p className="paragraph">
+              It seems like the page you were searching for does not exist. <br/>
+              <Link to="/">Go home</Link>
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
