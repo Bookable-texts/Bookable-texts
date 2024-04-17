ceb39995f9a8a504b079528ebb5bcb275572958a
Mofeleng <mofelengmokgope@gmail.com>
2024-04-17 13:43:42 +0200

    Styled the not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import PageNotFound from '../../public/PageNotFound.svg'
-import Link from 'react-router-dom'
+import PageNotFound from '../../public/pageNotFound.svg'
+import { Link } from 'react-router-dom'
+import '../styles/Notfound.css'
 
 function NotFound() {
   return (
@@ -10,13 +11,13 @@ function NotFound() {
         <div className="center">
           <img src={PageNotFound} alt="Page not found SVG" className='page_not_found_img' />
 
-          <div className="content">
+          <div className="content margin_top_large">
             <h3 className="heading small">
               Sorry we could not find the page you were looking for
             </h3>
             <p className="paragraph">
               It seems like the page you were searching for does not exist. <br/>
-              <Link to="/">Go home</Link>
+              <Link to="/" className='text_primary'>Go home</Link>
 
             </p>
           </div>
