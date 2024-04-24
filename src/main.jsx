321ff71b6bad4bb16ce9cf5878f6d7db60027aaa
Mofeleng <mofelengmokgope@gmail.com>
2024-04-24 14:15:34 +0200

    Added the routes to the signup and register pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
+import SignUp from './pages/SignUp'
+import Register from './pages/Register'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,6 +16,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={
           <Home />
+        } />
+        <Route path='/signup' element={
+          <SignUp />
+        } />
+        <Route path='/register' element={
+          <Register />
         } />
         <Route path='*' element={
           <NotFound />
